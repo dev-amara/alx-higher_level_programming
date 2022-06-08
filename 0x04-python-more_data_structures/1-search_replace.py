2edec39324c3e661ed0decca64cead2bb8756506
Amara Bamba <amara.dianney@gmail.com>
2022-06-08 22:04:28 +0000

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = [x if x != search else replace for x in my_list]
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
-
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
